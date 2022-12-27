fb15dc65268e1498515a290d35de73bf73bf4ea2
eswar <eswarlesnar@gmail.com>
2022-12-27 20:56:36 +0530

    added the routing functionality

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,9 +1,13 @@
 import styled from "styled-components"
-
+import {useState} from "react"
+import { useNavigate } from "react-router-dom";
 
 const HeaderWrapper = styled.header`
     padding : 0.01em ; 
     background : #89ffd2;
+    display : flex ;
+    justify-content : space-between;
+    align-items : center;
     `;
 
 const Title = styled.h3`
@@ -11,10 +15,28 @@ const Title = styled.h3`
     color :white;
     margin-left :2em;
     `
+const StyledButton = styled.button`
+    width : 100px;
+    height : 40px;
+    background : aqua;
+    text-align : center;
+    border : none;`
 const Header = () => {
+    const [view , setView ] = useState(true)
+    const navigate = useNavigate()
+    const handleClick = (e) => {
+        setView(!view)
+        if(view === true) {
+            navigate("/tasks")
+        }else{
+            navigate("/")
+        }
+    }
     return( 
     <HeaderWrapper>
         <Title>Board</Title>
+        {view === true ? <StyledButton  onClick = {e => handleClick(e)}>Task view</StyledButton>:
+        <StyledButton onClick = {e => handleClick(e)}>Board View</StyledButton>}
     </HeaderWrapper>)
 }
 
